6389de6f593c8685ab6688c5867582b48fdcc3e5
Domi <domi.hustinova@gmail.com>
2026-03-17 19:44:18 +0100

    test: replace Pixel with iPhone for mobile test

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,28 +13,17 @@ import { defineConfig, devices } from "@playwright/test"
  */
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: "http://localhost:3000",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     testIdAttribute: "data-test",
     screenshot: "on",
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "Desktop Chrome",
@@ -45,12 +34,10 @@ export default defineConfig({
       use: devices["Desktop Firefox"],
     },
     {
-      name: "Mobile Chrome",
-      use: devices["Pixel 5"],
+      name: "Mobile Safari",
+      use: devices["iPhone 12"],
     },
   ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
     url: "http://localhost:3000",
